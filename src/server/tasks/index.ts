8191bef511d62592008acd1144f1a9bad9f0e8dd
xuyang <xuyang@wozlla.com>
2015-07-12 03:18:36 +0800

    QZONE

--- src/server/tasks/index.ts
+++ src/server/tasks/index.ts
@@ -1,6 +1,6 @@
 /// <reference path="./interface.d.ts"/>
 import WanbaSDKLogs2Local = require('./WanbaSDKLogs2Local');
-import QzoneLatteLogs2Local = require('./QzoneLatteLogs2Local');
+import QzoneLatteLogs2Local = require('./QZoneLatteLogs2Local');
 export function run(){
   console.log('管理员校对服务器时间...');
   console.log("Now:"+new Date());
