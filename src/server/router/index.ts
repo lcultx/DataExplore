def70eafffdaa8a9cd9bb2608c47eae5eb698298
xuyang <xuyang@wozlla.com>
2015-07-15 21:09:19 +0800

    delete get_pay_point

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -3,7 +3,6 @@ import mogHelper = require('../../library/mogHelper');
 export function loadRouters(app){
   mogHelper.init(function(){
     app.get('/yesterday_events',require('./get_yesterday_events'));
-    app.get('/pay_point',require('./get_pay_point'));
   });
 
 
