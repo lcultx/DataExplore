26ba6e00ba3b5d9365995ef9413d3f8cf83b3f3e
xuyang <xuyang@wozlla.com>
2015-07-29 16:26:42 +0800

    修改datepicker

--- src/components/datepicker/DatePicker.ts
+++ src/components/datepicker/DatePicker.ts
@@ -61,10 +61,10 @@ class Datepicker extends EventEmitter{
               format: 'YYYY/MM/DD'
           },
           ranges: {
-             'Today': [moment(), moment()],
+          //   'Today': [moment(), moment()],
              'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-             'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-             'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+             'Last 7 Days': [moment().subtract(7, 'days'),moment().subtract(1, 'days')],
+             'Last 30 Days': [moment().subtract(30, 'days'), moment().subtract(1, 'days')],
              'This Month': [moment().startOf('month'), moment().endOf('month')],
              'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
           }
